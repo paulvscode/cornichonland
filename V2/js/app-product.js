ec6a2bfc80a0f08fecb2e4228d27811f1eda0420
paulvscode <paulbdelagerie@gmail.com>
2020-10-24 11:46:47 +0200

    fixed js add remove product

--- V2/js/app-product.js
+++ V2/js/app-product.js
@@ -1,12 +1,22 @@
 // ajout produit page html products
 
 var button = document.getElementById("btn");
+button.addEventListener("click", addingArticle)
 
-button.addEventListener("click", function(event) {
+function addingArticle() {
     var addArticle = document.querySelector(".section__products__article");
     var parentDivArticle = document.getElementById("parent-article-div");
     var cln = addArticle.cloneNode(true);
     parentDivArticle.appendChild(cln);
-    }
-); 
+};
+
+var buttonRemove = document.getElementById("btn-remove");
+buttonRemove.addEventListener("click", removingArticle)
+
+
+function removingArticle() {
+    var select = document.getElementById("parent-article-div");
+    select.removeChild(select.lastElementChild);
+}
+
 
